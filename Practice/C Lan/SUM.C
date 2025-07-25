7c919eff33b5436affc0fb0b81121e1746646065
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-07-25 01:25:17 -0700

    first commit

---
+++ Practice/C Lan/SUM.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+int a,b,c;
+clrscr();
+printf("\nenter A");
+scanf("%d",&a);
+printf("\nenter B");
+scanf("%d",&b);
+if(a>b)
+
+{
+  printf("A is Greater");
+}
+
+else{
+printf("b is great");
+}
+getch();
+
+}
+
+
--- Practice/C Lan/hello.c
+++ Practice/C Lan/hello.c
@@ -1,18 +1,15 @@
+/*******************************************
+Statement - Print Hello World
+Programmer - Vineet Choudhary
+Written For - http://developerinsider.in
+********************************************/
+
 #include <stdio.h>
 #include <conio.h>
 
-int main()
+void main()
 {
-    int a, b, c, sum;
-
-    printf("Enter A\n");
-    scanf("%d", &a);
-    printf("Enter B\n");
-    scanf("%d", &b);
-    printf("Enter C\n");
-    scanf("%d", &c);
-    sum = a + b + c;
-    printf("Sum:%d", sum);
-    getch();
-    return 0;
+	clrscr();
+	printf("Hello world\n");
+	getch();
 }
